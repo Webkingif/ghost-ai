cef611600d778ae17dca9ae8dfeeba613f2a07bd
Webkingif <webkingif.other@gmail.com>
2026-07-12 21:33:03 +0100

    sidebar content no longer accessible when fixed

--- components/editor/project-sidebar.tsx
+++ components/editor/project-sidebar.tsx
@@ -16,11 +16,15 @@ function ProjectSidebar({ isOpen, onClose }: ProjectSidebarProps) {
     <>
       {isOpen && (
         <div
+          inert={!isOpen}
+          aria-hidden={!isOpen}
           className="fixed inset-0 z-40 bg-black/50"
           onClick={onClose}
         />
       )}
       <aside
+        inert={!isOpen}
+        aria-hidden={!isOpen}
         className={cn(
           "fixed inset-y-0 left-0 z-50 flex w-72 flex-col border-r border-border bg-card transition-transform duration-200",
           isOpen ? "translate-x-0" : "-translate-x-full"
